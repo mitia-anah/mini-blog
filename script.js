3ada09a008ae7452afac1a2e731c13d8ed47dfa7
Rosny Ratsianompo <rosny.rat@onja.org>
2020-07-04 17:04:36 +0100

    had just start working on checkbox(not finished yet)

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-console.log('it works');
-
 // code your solution in here
-
-const myDiv = document.getElementById('post-list');
-const header = document.querySelector('#new-post-title');
-const paragraph = document.querySelector('#new-post-content');
-
-
-    // Get access to DOM elements
+ 
+//  
+    const myDiv = document.getElementById('post-list');
+    const header = document.querySelector('#new-post-title');
+    const paragraph = document.querySelector('#new-post-content');
 
     const divSomeSpace= document.createElement('div');
     const divCardBody = document.createElement('div');
     const newHeader = document.createElement('h5');
     const newParagraph = document.createElement('p');
-
-
 
     divSomeSpace.classList.add('card', 'some-space');
     divCardBody.classList.add('card-body');
@@ -25,16 +19,21 @@ const paragraph = document.querySelector('#new-post-content');
     divCardBody.appendChild(newParagraph);
     divSomeSpace.appendChild(divCardBody);
 
-
     const submit = document.querySelector('#submit-form'); 
     submit.addEventListener('click', ($event) => {
     $event.preventDefault();
     newHeader.textContent = `${header.value}`;
     newParagraph.textContent = `${paragraph.value}`;
     myDiv.appendChild(divSomeSpace);
-    })
-   
+    });
+
+// checkBox
+    const hobbiesTag = document.getElementById('hobbies-tag');
+    const schoolLifeTag = document.getElementById('school-life-tag');
+    const personalTag = document.getElementById('presonal-tag');
 
 
+    hobbiesCheckbox.addEventListener('change', ($event) => {
 
+    });
 
